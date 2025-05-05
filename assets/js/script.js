c70e3997aff81d0c3430553706da959a8029477c
Chris514 <hal225@lehigh.edu>
2025-05-05 12:35:18 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // 页面加载时的动画效果
+   
     $('.avatar').hide().fadeIn(1000);
     $('.intro-section h1').hide().fadeIn(1200);
     $('.intro-section h2').hide().fadeIn(1400);
@@ -7,13 +7,13 @@ $(document).ready(function() {
     $('.description').hide().fadeIn(1800);
     $('.social-links').hide().fadeIn(2000);
 
-    // 社交链接点击效果
+    
     $('.social-link').on('click', function(e) {
         e.preventDefault();
         $(this).addClass('active').siblings().removeClass('active');
     });
 
-    // 平滑滚动效果
+   
     $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
         var target = $(this.hash);
@@ -24,13 +24,13 @@ $(document).ready(function() {
         }
     });
 
-    // 视差滚动效果
+    
     $(window).on('scroll', function() {
         var scrollTop = $(window).scrollTop();
         $('body::before').css('transform', 'translateY(' + (scrollTop * 0.5) + 'px)');
     });
 
-    // 段落渐入效果
+    
     function checkFadeIn() {
         $('.statement-paragraph').each(function() {
             var elementTop = $(this).offset().top;
@@ -44,9 +44,9 @@ $(document).ready(function() {
         });
     }
 
-    // 初始检查
+   
     checkFadeIn();
     
-    // 滚动时检查
+    
     $(window).on('scroll', checkFadeIn);
 }); 
